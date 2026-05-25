7e607b1b8ad37c41b1dbbed7c0eefcda697d3ff0
Ehtisham-Ahmad95 <ranaashamiii@gmail.com>
2026-05-25 15:54:34 +0500

    Add files via upload

---
+++ PF Week 13 - Lab/task 5.py
@@ -0,0 +1,5 @@
+s={"CE","CS","EE","MME","CE"}
+for i in s:
+    print(i)
+if "CE" in s:
+   print("CE Exist in the set")
---
+++ PF Week 13 - Lab/task 6.py
@@ -0,0 +1,7 @@
+s={"QASIM","BILAL","NOMAN","SHAHZAIB","SHAYAN"}
+a=input("Enter Student name=").upper()
+if a in s:
+    print("YES student is present")
+else:
+    print("NOT present")
+    
