a51e27d7e0329aa744f21a92164d633868a0fb2c
Ehtisham-Ahmad95 <ranaashamiii@gmail.com>
2026-06-09 16:01:31 +0500

    Delete PF Week 11 - lab directory

--- PF Week 11 - lab/task 5.py
+++
@@ -1,4 +0,0 @@
-for i in range(1,4):
-    for j in range(i+1):
-        print("(",i,",",j,")",end=" ")
-        print("i",end=" ")
--- PF Week 11 - lab/task 7.py
+++
@@ -1,6 +0,0 @@
-for i in range(1,5):
-    for j in range(5-i):
-        print(" ", end="")
-    for k in range(2*i):
-        print("*", end="")
-    print()
