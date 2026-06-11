1d9a9b20f0a3956f9e623734b9aa9093fee890cf
Ehtisham-Ahmad95 <ranaashamiii@gmail.com>
2026-06-11 10:42:00 +0500

    Add files via upload

---
+++ PF(Exception and Handling)/task 1.py
@@ -0,0 +1,12 @@
+#Try means first to do 
+# except means if try makes an error then run excet 
+
+try:
+   b=int(input("Enter a number:"))
+   print(b)
+except:
+      print("invalid")
+print ("Hello World")
+
+
+
