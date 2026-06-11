415da790d0feac03db6d64ff3d4508d72d2f8465
Ehtisham-Ahmad95 <ranaashamiii@gmail.com>
2026-06-11 10:40:38 +0500

    Add files via upload

---
+++ PF(list,tuple,dictionary)/task 1.py
@@ -0,0 +1,4 @@
+a=[1,"Ali",2.1]
+print(a)
+print(a.append("bilal"))
+print(a)
---
+++ PF(list,tuple,dictionary)/task 3.py
@@ -0,0 +1,7 @@
+# Creating a tuple
+student = ("Ali", 18, "Computer Science")
+
+print(student[0])  
+print(student[1]) 
+
+# Tuples are immutable (cannot be changed)
